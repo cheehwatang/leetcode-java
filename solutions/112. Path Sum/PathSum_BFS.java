0a0631e84831d4aea748a2444938bae146681eca
Tang Chee Hwa <cheehwa.tang@gmail.com>
2023-01-21 06:21:27 +0800

    Update 112. Path Sum (Breadth-First Search)

--- solutions/112. Path Sum/PathSum_BFS.java
+++ solutions/112. Path Sum/PathSum_BFS.java
@@ -45,29 +45,23 @@ public class PathSum_BFS {
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
 
+        // If there are nodes in the 'queue', continue to traverse the tree until all nodes are visited.
         while (!queue.isEmpty()) {
-            // Remember to record the size of the 'queue', which is the size of the current level, using a variable.
-            // If we use "i < queue.size()" as the condition to break out of the loop,
-            // it might result in the inaccurate recording of the node values for each level,
-            // as the queue.size() is changing as we offer() or poll() nodes.
-            for (int i = 0, size = queue.size(); i < size; i++) {
-                // Remove the node from the 'queue'.
-                TreeNode currentNode = queue.poll();
+            TreeNode currentNode = queue.poll();
 
-                // If the path sum of the leaf node is equal to the 'targetSum', return true.
-                if (currentNode.left == null && currentNode.right == null && currentNode.val == targetSum)
-                    return true;
+            // If the path sum of the leaf node is equal to the 'targetSum', return true.
+            if (currentNode.left == null && currentNode.right == null && currentNode.val == targetSum)
+                return true;
 
-                // Add the value of the 'currentNode' to the left and right child, if not null.
-                // Add the children to the 'queue'.
-                if (currentNode.left != null) {
-                    currentNode.left.val += currentNode.val;
-                    queue.offer(currentNode.left);
-                }
-                if (currentNode.right != null) {
-                    currentNode.right.val += currentNode.val;
-                    queue.offer(currentNode.right);
-                }
+            // Add the value of the 'currentNode' to the left and right child, if not null.
+            // Add the children to the 'queue'.
+            if (currentNode.left != null) {
+                currentNode.left.val += currentNode.val;
+                queue.offer(currentNode.left);
+            }
+            if (currentNode.right != null) {
+                currentNode.right.val += currentNode.val;
+                queue.offer(currentNode.right);
             }
         }
         // If traverse the whole binary tree yet not found the path sum, return false.
