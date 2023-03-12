77cb5e0a8a68936bcdad95a10e54c06b0beeb2a2
Tang Chee Hwa <cheehwa.tang@gmail.com>
2023-03-12 11:23:08 +0800

    344. Reverse String

--- solutions/344. Reverse String/ReverseString_TwoPointers.java
+++ solutions/344. Reverse String/ReverseString_TwoPointers.java
@@ -1,24 +1,11 @@
 package com.cheehwatang.leetcode;
 
-/**
- * Problem:
- * Given a character array 's', reverse the elements arrangement by modifying the input array in-place.
- *
- * Note:
- * Use two pointers.
- *
- *
- * Example 1:
- * Input : s = ["h","e","l","l","o"]
- * Output: ["o","l","l","e","h"]
- *
- * Example 2:
- * Input : s = ["L","e","e","t","C","o","d","e"]
- * Output: ["e","d","o","C","t","e","e","L"]
- *
- *
- * @author Chee Hwa Tang
- */
+// Time Complexity  : O(n),
+// where 'n' is the length of the char array 's'.
+// We traverse half of the array once to swap the characters, which scales linearly with the length of the char array 's'.
+//
+// Space Complexity  : O(1),
+// as the auxiliary space used is independent on the length of the char array 's'.
 
 public class ReverseString_TwoPointers {
 
@@ -26,14 +13,11 @@ public class ReverseString_TwoPointers {
     // Use two pointers, one from the left, the other from the right, and swap the characters.
 
     public void reverseString(char[] s) {
-
         // We only need to traverse until halfway, since we are swapping the left and right.
         // Traversing the whole length will result in the original array, since we swap twice.
         for (int index = 0; index < s.length / 2; index++) {
-
             int left = index;
             int right = s.length - index - 1;
-
             // Optional to check if both characters are the same.
             // Or we can just swap the left and right characters without checking.
             if (s[left] != s[right]) {
@@ -43,5 +27,4 @@ public class ReverseString_TwoPointers {
             }
         }
     }
-
 }
