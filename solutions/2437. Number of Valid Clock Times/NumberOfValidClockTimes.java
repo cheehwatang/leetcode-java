cdab656b14287f52d11b0571eca2d67d3d3c9291
Tang Chee Hwa <cheehwa.tang@gmail.com>
2023-04-07 09:39:27 +0800

    2437. Number of Valid Clock Times

--- solutions/2437. Number of Valid Clock Times/NumberOfValidClockTimes.java
+++ solutions/2437. Number of Valid Clock Times/NumberOfValidClockTimes.java
@@ -1,32 +1,10 @@
 package com.cheehwatang.leetcode;
 
-/**
- * Problem:
- * Given a string 'time' with the format "hh:mm", with the time ranges from "00:00" to "23:59".
- * In 'time', some number digits are represented by a '?' unknown symbol, which can be replaced with a digit from 0 to 9.
- * Return the number of valid clock times that can be created by replacing every '?' in 'time'.
- *
- *
- * Example 1:
- * Input    : time = "??:??"
- * Output   : "1440"
- * Explanation: Possible choices for the hours are 24, and for minutes are 60, totalling 24 * 60 = 1440.
- *
- *
- * Example 2:
- * Input    : time = "?9:00"
- * Output   : "2"
- * Explanation: With the second digit of hour greater than 3, only '0' and '1' is possible, since hour 29 is invalid.
- *
- *
- * Example 3:
- * Input    : time = "00:?0"
- * Output   : "6"
- * Explanation: For the minutes, the first digit could possibly be from number 0 to 5.
- *
- * 
- * @author Chee Hwa Tang
- */
+// Time Complexity  : O(1),
+// as the number of operations performed is independent of the input 'time'.
+//
+// Space Complexity : O(1),
+// as the auxiliary space used is independent of the input 'time'.
 
 public class NumberOfValidClockTimes {
 
@@ -48,19 +26,27 @@ public class NumberOfValidClockTimes {
 
         // Section to check the hours.
         int hours = 1;
+        // ?? = 24 possibilities.
         if (hourMinute[0] == '?' && hourMinute[1] == '?') hours = 24;
         else if (hourMinute[0] == '?') {
+            // ?3 (0 to 3) = 3 possibilities where ? = 0, 1 or 2.
             if (hourMinute[1] - '0' <= 3) hours = 3;
+            // ?9 (4 to 9) = 2 possibilities where ? = 0 or 1.
             else hours = 2;
         } else if (hourMinute[1] == '?') {
+            // 2? = 4 possibilities where ? = 0, 1, 2 or 3.
             if (hourMinute[0] == '2') hours = 4;
+            // (0 or 1) 1? = 10 possibilities where ? = 0 to 9.
             else hours = 10;
         }
 
         // Section to check the minutes.
         int minutes = 1;
+        // ?? = 60 possibilities.
         if (hourMinute[3] == '?' && hourMinute[4] == '?') minutes = 60;
+        // ?0 = 6 possibilities where ? = 0 to 5.
         else if (hourMinute[3] == '?') minutes = 6;
+        // 0? = 10 possibilities where ? = 0 to 9.
         else if (hourMinute[4] == '?') minutes = 10;
 
         return hours * minutes;
