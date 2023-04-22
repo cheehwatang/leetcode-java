578ccfb253655b96ab5bcf8d21c799e62bb43e14
Tang Chee Hwa <cheehwa.tang@gmail.com>
2023-04-22 12:50:04 +0800

    151. Reverse Words in a String (Collections)

--- solutions/151. Reverse Words in a String/ReverseWordsInAString_Collections.java
+++ solutions/151. Reverse Words in a String/ReverseWordsInAString_Collections.java
@@ -4,26 +4,15 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-/**
- * Problem:
- * Given a string 's', reverse order of the words, where each word is separated by at least one space.
- * Return a string of words in reverse order concatenated by a single space.
- * Please exclude any additional spaces as well as any leading or trailing spaces.
- *
- *
- * Example 1:
- * Input : s = "LeetCode is awesome"
- * Output: "awesome is LeetCode"
- *
- * Example 2:
- * Input : s = "   Hello   World   "
- * Output: "World Hello"
- * Explanation: The reversed string do not contain any leading or trailing spaces,
- *              and reduce multiple spaces to a single space between words.
- *
- *
- * @author Chee Hwa Tang
- */
+// Time Complexity  : O(n),
+// where 'n' is the length of string 's'.
+// The string split() method, the Collections.reverse() method, and String.join() method
+// has a time complexity of O(n).
+//
+// Space Complexity : O(n),
+// where 'n' is the length of string 's'.
+// The splitting of string 's' into array of 'words', and the final result string
+// requires memory that grows linearly with the size of the input string 's'.
 
 public class ReverseWordsInAString_Collections {
 
@@ -33,9 +22,14 @@ public class ReverseWordsInAString_Collections {
     // Use the String.join with " " as the delimiter to produce the reversedWords.
 
     public static String reverseWords(String s) {
+        // .trim() removes leading and trailing spaces.
+        // "\\s+" is regex for spaces (one or more).
         List<String> words = Arrays.asList(s.trim().split("\\s+"));
+
+        // Reverse the strings in the 'words' list.
         Collections.reverse(words);
+
+        // Join the words with a single space between the words.
         return String.join(" ", words);
     }
-
 }
