f7e06d900da48eb0588d3acd2863ac4951f5371a
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-05-24 15:37:05 +0800

    1480. Running Sum of 1d Array

--- solutions/1480. Running Sum of 1D Array/RunningSumOf1DArray.java
+++ solutions/1480. Running Sum of 1D Array/RunningSumOf1DArray.java
@@ -1,29 +1,19 @@
 package com.cheehwatang.leetcode;
 
-/**
- * Problem:
- * Given an array 'nums', return the running sum of 'nums'.
- *
- *
- * Example 1:
- * Input : nums = [1,2,3]
- * Output: [1,3,6]
- *
- * Example 2:
- * Input : nums = [1,-2,3,-4,5,-6]
- * Output: [1,-1,2,-2,3,-3]
- *
- *
- * @author Chee Hwa Tang
- */
+// Time Complexity  : O(n),
+// where 'n' is the length of 'nums'.
+// We traverse the 'nums' array once to get the running sum.
+//
+// Space Complexity : O(1),
+// as the auxiliary space used is independent of 'nums'.
 
-// Time Complexity = O(N)
-// Space Complexity = O(1)
-// where N = nums.length
 public class RunningSumOf1DArray {
 
-    public int[] runningSum(int[] nums) {
+    // Approach:
+    // Starting at index 1,
+    // continue to add the previous number while replacing the number in 'nums' array in-place.
 
+    public int[] runningSum(int[] nums) {
         // In-place update of the sum of current number nums[i] with the previous number nums[i-1].
         for (int i = 1; i < nums.length; i++) {
             nums[i] += nums[i-1];
