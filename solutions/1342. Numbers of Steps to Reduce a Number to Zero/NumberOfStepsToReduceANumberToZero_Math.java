3958325296d926e38ca374bffc9a04bff552c977
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-06-02 14:16:35 +0800

    1342. Number of Steps to Reduce a Number to Zero (Math)

--- solutions/1342. Numbers of Steps to Reduce a Number to Zero/NumberOfStepsToReduceANumberToZero_Math.java
+++ solutions/1342. Numbers of Steps to Reduce a Number to Zero/NumberOfStepsToReduceANumberToZero_Math.java
@@ -1,43 +1,19 @@
 package com.cheehwatang.leetcode;
 
-/**
- * Problem:
- * Given an integer, return the number of steps to reduce it to zero.
- * In each step,
- * 1. If the current number is even, divide it by 2.
- * 2. If the current number is odd, subtract 1 from it.
- *
- * Note:
- * Use math or bit manipulation to solve.
- *
- *
- * Example 1:
- * Input : 12
- * Output: 5
- * Explanation:
- * Step 1 - 12 is even, divide by 2 to get 6.
- * Step 2 - 6 is even, divide by 2 to get 3.
- * Step 3 - 3 is odd, subtract 1 to get 2.
- * Step 4 - 2 is even, divide by 2 to get 1.
- * Step 5 - 1 is odd, subtract 1 to get 0.
- *
- * Example 2:
- * Input : 5
- * Output: 4
- * Explanation:
- * Step 1 - 5 is odd, subtract 1 to get 4.
- * Step 2 - 4 is even, divide by 2 to get 2.
- * Step 3 - 2 is even, divide by 2 to get 1.
- * Step 4 - 1 is odd, subtract 1 to get 0.
- *
- *
- * @author Chee Hwa Tang
- */
+// Time Complexity  : O(logn),
+// where 'n' is 'num'.
+// Since we are dividing the number by 2 when it is even, we can approximate the time complexity as O(logn),
+// considering it similar to binary search.
+//
+// Space Complexity : O(1),
+// as the auxiliary space used is independent of the input.
 
 public class NumberOfStepsToReduceANumberToZero_Math {
 
-    public int numberOfSteps(int num) {
+    // Approach:
+    // The intuitive approach, to subtract by 1 when odd or divide by 2 when even.
 
+    public int numberOfSteps(int num) {
         int count = 0;
         while (num > 0) {
             num = num % 2 == 0 ? num / 2 : num - 1;
