b16708e10dc18090332d984d652e401b9760b643
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-06-15 13:07:28 +0800

    1679. Max Number of K-Sum Pairs (Hash Table)

--- solutions/1679. Max Number of K-Sum Pairs/MaxNumberOfKSumPairs_HashTable.java
+++ solutions/1679. Max Number of K-Sum Pairs/MaxNumberOfKSumPairs_HashTable.java
@@ -3,31 +3,13 @@ package com.cheehwatang.leetcode;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Problem:
- * Given an array of integers 'nums' and an integer 'k'.
- * In one operation, you can pick two numbers from the array whose sum equals 'k' and remove them from the array.
- * Return the maximum number of operations that can be performed on the array.
- *
- *
- * Example 1:
- * Input    : nums = [1,2,3,4,5], k = 6
- * Output   : 2
- * Explanation:
- * 1. Remove 1 and 5, nums = [2,3,4].
- * 2. Remove 2 and 4, nums = [3].
- * No more pairs left to get a sum of 6.
- *
- *
- * Example 2:
- * Input    : nums = [1,2,3,4,5], k = 2
- * Output   : 0
- * The unique pairs with k difference:
- * No more pair can get a sum of 2.
- *
- *
- * @author Chee Hwa Tang
- */
+// Time Complexity  : O(n),
+// where 'n' is the length of 'nums'.
+// We traverse 'nums' array once to find the match.
+//
+// Space Complexity : O(n),
+// where 'n' is the length of 'nums'.
+// The HashMap has a maximum size of 'n', where all numbers are unique and no match is found.
 
 public class MaxNumberOfKSumPairs_HashTable {
 
@@ -42,7 +24,6 @@ public class MaxNumberOfKSumPairs_HashTable {
 
         // For each number in 'nums',
         for (int number : nums) {
-
             // 'difference' is the second number that pairs with 'number' to sum to 'k'.
             int difference = k - number;
 
@@ -53,17 +34,14 @@ public class MaxNumberOfKSumPairs_HashTable {
             // Increase the count, and reduce the frequency or remove the entry.
             if (map.containsKey(difference)) {
                 count++;
-                if (map.get(difference) == 1) {
+                if (map.get(difference) == 1)
                     map.remove(difference);
-                }
-                else {
+                else
                     map.put(difference, map.get(difference) - 1);
-                }
             }
             // If no match found, increase the frequency or add the entry into the HashMap.
-            else {
+            else
                 map.put(number, map.getOrDefault(number, 0) + 1);
-            }
         }
         return count;
     }
